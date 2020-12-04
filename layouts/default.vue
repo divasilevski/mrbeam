5e81bd940b579024a0789dcc8d1ab3edffbd962f
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2020-12-04 23:42:25 +0300

    init commit

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  #app
+    Nuxt
+</template>
+
+<style lang="scss">
+#app {
+  background: $main;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  .container
+    h1 Hello world!
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss"></style>
