ffd4e7e61607932979134f68f424fb391a904eef
Дмитрий Василевский <dmitry.vasilevsky@immelman.ru>
2023-04-26 17:30:32 +0300

    Add calculate and generate api

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <pre>{{ generatedData }}</pre>
+  <pre>{{ data }}</pre>
+</template>
+
+<script lang="ts" setup>
+const { data: generatedData } = await useFetch('/api/generate', {
+  server: true,
+})
+const { data } = await useFetch('/api/calculate', {
+  method: 'post',
+  body: unref(generatedData),
+  server: true,
+})
+</script>
