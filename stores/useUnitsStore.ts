5c21d642191782cae382e97e42e2229cdff7eb14
Дмитрий Василевский <dmitry.vasilevsky@immelman.ru>
2023-04-28 16:44:33 +0300

    Add units store

---
+++ stores/useUnitsStore.ts
@@ -0,0 +1,65 @@
+import { defineStore } from 'pinia'
+
+export const useUnitsStore = defineStore('units-store', () => {
+  const units = ref<Unit[]>([])
+  const solution = ref<Solution | null>(null)
+  const solutionError = ref<string | null>(null)
+
+  const add = (unit: Unit) => {
+    units.value = [...units.value, unit]
+  }
+
+  const set = (newUnits: Unit[]) => {
+    units.value = newUnits
+  }
+
+  const clear = () => {
+    units.value = []
+  }
+
+  const removeById = (id: string) => {
+    units.value = units.value.filter((unit: Unit) => unit.id !== id)
+  }
+
+  const generateAsync = async () => {
+    const { data } = await useFetch('/api/generate')
+
+    if (data.value) {
+      units.value = JSON.parse(data.value)
+    }
+  }
+
+  const calculateAsync = async () => {
+    if (units.value.length > 0) {
+      const { data, error } = await useFetch('/api/calculate', {
+        method: 'post',
+        body: units.value,
+      })
+
+      if (data.value) {
+        solution.value = JSON.parse(data.value)
+      }
+
+      if (error.value) {
+        solutionError.value = error.value.data.message
+      }
+    }
+  }
+
+  watch(units, () => {
+    solution.value = null
+    solutionError.value = null
+  })
+
+  return {
+    units,
+    solution,
+    solutionError,
+    add,
+    set,
+    clear,
+    removeById,
+    generateAsync,
+    calculateAsync,
+  }
+})
