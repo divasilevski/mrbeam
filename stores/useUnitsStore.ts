d3e548689e42dd3a2e0d84966d00aec5abb699d6
Дмитрий Василевский <dmitry.vasilevsky@immelman.ru>
2023-05-18 20:17:34 +0300

    Add useIDBKeyval to store

--- stores/useUnitsStore.ts
+++ stores/useUnitsStore.ts
@@ -1,11 +1,12 @@
 import { defineStore } from 'pinia'
+import { useIDBKeyval } from '@vueuse/integrations/useIDBKeyval'
 import { useSolutionStore } from '~/stores/useSolutionStore'
 import generateSimple from '~/utils/fem/generateSimple'
 
 export const useUnitsStore = defineStore('units-store', () => {
   const store = useSolutionStore()
 
-  const units = ref<Unit[]>([])
+  const { data: units } = useIDBKeyval<Unit[]>('units-store', [])
 
   const add = (unit: Unit) => {
     units.value = [...units.value, unit]
