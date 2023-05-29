6a3d721229c670f4475f902d6a878275806be85c
Дмитрий Василевский <dmitry.vasilevsky@immelman.ru>
2023-05-29 17:14:39 +0300

    Remove about

--- pages/about.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <article class="prose prose-slate max-w-2xl">
-    <VueComponent />
-  </article>
-</template>
-
-<script lang="ts" setup>
-import { VueComponent } from '~/content/about.md'
-</script>
