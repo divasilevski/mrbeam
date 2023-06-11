81bd080b85292826083cc1f53ed0ea42e0ce6beb
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2023-06-11 16:08:56 +0300

    Add force alive component

---
+++ components/Shared/ForceAlive.vue
@@ -0,0 +1,18 @@
+<template>
+  <slot :key="key" />
+</template>
+
+<script lang="ts" setup>
+const key = ref(0)
+const wasDeactivated = ref(false)
+
+onActivated(() => {
+  if (wasDeactivated.value) {
+    key.value++
+  }
+})
+
+onDeactivated(() => {
+  wasDeactivated.value = true
+})
+</script>
