8cf2494d24bbcccd4aa02200955601d0a4947f18
Дмитрий Василевский <dmitry.vasilevsky@immelman.ru>
2025-01-10 12:37:06 +0300

    Change vue gtm plugin to native plugin - Using GTMDataLayer for experiment

--- plugins/gtm.ts
+++ plugins/gtm.ts
@@ -1,11 +1,13 @@
-import { createGtm } from '@gtm-support/vue-gtm'
-import type { VueGtmUseOptions } from '@gtm-support/vue-gtm'
-
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(
-    createGtm({
-      id: 'GTM-KV6RHZS',
-      vueRouter: useRouter() as VueGtmUseOptions['vueRouter'],
-    }),
-  )
+  const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','GTMDataLayer','GTM-KV6RHZS');`
+  const gtmNoscript = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KV6RHZS height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+
+  nuxtApp.vueApp.use({
+    install() {
+      useHead({
+        script: [{ innerHTML: gtmScript }],
+        noscript: [{ innerHTML: gtmNoscript }],
+      })
+    },
+  })
 })
